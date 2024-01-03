ac8ed317519e37b9541bd064d500ad21e8cb8fbd
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-01-03 15:25:24 +0300

    Fix typos pdftk

--- appconfig/latex/install.sh
+++ appconfig/latex/install.sh
@@ -41,7 +41,7 @@ while true; do
     sudo apt-get -y install texlive texlive-latex-extra texlive-lang-czechslovak texlive-science texlive-pstricks latexmk texmaker texlive-font-utils texlive-fonts-extra texlive-bibtex-extra biber okular pdf-presenter-console dvipng sketch
 
     if [ -n "$FOCAL" ] || [ -n "$JAMMY" ]; then
-      sudo apt-get -y pdftk
+      sudo apt install -y pdftk
     fi
 
     # use in pdfpc to play videos
