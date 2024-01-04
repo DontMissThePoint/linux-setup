8e212c3270a431944eaf64fbaea4f2ee0bb2f1b0
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-01-04 12:33:00 +0300

    Fix typo
    - Work in progress

--- appconfig/docker/install.sh
+++ appconfig/docker/install.sh
@@ -41,7 +41,7 @@ while true; do
     sudo apt-get update
     sudo apt-get install -y ca-certificates curl gnupg
     sudo install -m 0755 -d /etc/apt/keyrings
-    surl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
     # Add the repository to apt sources
