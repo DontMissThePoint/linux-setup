9d0463de59641fa58ce29dd4376a7c5a462717c3
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-01-11 16:29:45 +0300

    Add change cursor style neovim modes
    - Update blinkon milliseconds

--- appconfig/nvim/lua/custom/init_git.lua
+++ appconfig/nvim/lua/custom/init_git.lua
@@ -7,7 +7,9 @@ autocmd("VimResized", {
 })
 
 -- Unset guicursor whenever it is changed
-vim.cmd[[autocmd OptionSet * noautocmd set guicursor=]]
+vim.cmd[[autocmd OptionSet * noautocmd set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
+		  \,a:blinkwait750-blinkoff150-blinkon175-Cursor/lCursor
+		  \,sm:block-blinkwait175-blinkoff150-blinkon175]]
 
 -- Status
 if vim.fn.exists('$TMUX') then
--- scripts/mega-sync.sh
+++ scripts/mega-sync.sh
@@ -5,7 +5,7 @@
 LOG1="$HOME/.purple/logs/whatsapp/256782564488@s.whatsapp.net"
 LOG2="$HOME/.purple/logs/whatsapp/256759879191@s.whatsapp.net"
 
-rclone sync -LPMu --fast-list --transfers 16 --checkers 16 --ignore-errors ~/linux-setup Mega:/linux-setup
+rclone sync -LPMu --fast-list --transfers 25 --checkers 16 --ignore-errors ~/linux-setup Mega:/linux-setup
 
 echo "Sync chat data..."
 rclone copy -LPMu "$LOG1" Mega:/00.Data/256782564488@s.whatsapp.net && find "$LOG1" -type f -iregex '.*\.\(jpg\|txt\|png\|html\)$' | xargs rm -f
