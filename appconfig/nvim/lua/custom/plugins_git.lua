f80073af0ed045fc65cfc6de75296db70601d9b0
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-02-17 18:12:54 +0300

    Add quickfix trouble nvim

--- appconfig/nvim/lua/custom/plugins_git.lua
+++ appconfig/nvim/lua/custom/plugins_git.lua
@@ -135,6 +135,17 @@ local plugins = {
     }
   },
 
+  {
+     "folke/trouble.nvim",
+     event = "VeryLazy",
+     dependencies = { "nvim-tree/nvim-web-devicons" },
+     opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+     }
+  },
+
   { "alexghergh/nvim-tmux-navigation",
     lazy = false,
     config = function()
