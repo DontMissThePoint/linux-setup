00b3d7c968a956e06fcd32d86f28ea915fd46491
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-04-03 16:25:30 +0300

    Update configuration files custom search

--- appconfig/brew/install.sh
+++ appconfig/brew/install.sh
@@ -59,10 +59,11 @@ while true; do
     brew cleanup --prune=all
 
     # configs
-    mkdir -p ~/.config/gitui ~/.config/btop ~/.config/bat ~/.aria2 ~/.config/aria2 ~/.config/glow
+    mkdir -p ~/.config/gitui ~/.config/btop ~/.config/bat ~/.aria2 ~/.config/aria2 ~/.config/glow ~/.config/s
     cp "$APP_PATH/key_bindings.ron" ~/.config/gitui/key_bindings.ron
     cp "$APP_PATH/btop.conf" ~/.config/btop/btop.conf
-    cp "$APP_PATH/config" ~/.config/bat/config
+    cp "$APP_PATH/bat.config" ~/.config/bat/config
+    cp "$APP_PATH/s.config" ~/.config/s/config
     cp "$APP_PATH/aria2.conf" ~/.config/aria2/aria2.conf
     cp "$APP_PATH/glow.yml" ~/.config/glow/glow.yml
 
