d1ba355f6e871eb140cd132fb0b6093d50918f1c
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-05-14 16:18:43 +0300

    Update development scripts: i3-layout

--- scripts/dotsync.sh
+++ scripts/dotsync.sh
@@ -9,7 +9,6 @@ cp $GIT_PATH/linux-setup/appconfig/vim/dotvimrc $HOME/ownCloud/Documents/ACCOMPL
 cp $GIT_PATH/linux-setup/appconfig/zsh/dotzshrc_template $HOME/ownCloud/Documents/ACCOMPLISHED/dotfiles/dotzshrc_template
 cp $GIT_PATH/linux-setup/scripts/dotsync.sh $HOME/ownCloud/Documents/ACCOMPLISHED/dotfiles/dotsync.sh
 cp $GIT_PATH/linux-setup/appconfig/go-whatsapp/dotgntrc $HOME/ownCloud/Documents/ACCOMPLISHED/dotfiles/.gntrc
-cp $GIT_PATH/linux-setup/docs/dotcards $HOME/ownCloud/Documents/ACCOMPLISHED/dotfiles/.cards.txt
 cp $GIT_PATH/linux-setup/appconfig/fzf/dotFZF_DEFAULT_OPTS $HOME/ownCloud/Documents/ACCOMPLISHED/dotfiles/.FZF_DEFAULT_OPTS
 cp $GIT_PATH/linux-setup/appconfig/qutebrowser/config_template.py $HOME/ownCloud/Documents/ACCOMPLISHED/dotfiles/config.py
 cp $GIT_PATH/linux-setup/appconfig/zathura/zathurarc $HOME/ownCloud/Documents/ACCOMPLISHED/dotfiles/zathurarc
