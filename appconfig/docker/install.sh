939101732b83fd486867cfbb4118d49dbe07e887
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-06-01 18:50:41 +0300

    Critical hotfix: i3bar dock mode

--- appconfig/docker/install.sh
+++ appconfig/docker/install.sh
@@ -111,7 +111,7 @@ http://pub.freerdp.com/repositories/deb/"$(. /etc/os-release && echo "$VERSION_C
     # desktop scale factor. This value MUST be ignored if it is less than 100%
     # or greater than 500% or deviceScaleFactor is not 100%, 140%, or 180%.
 
-    # /opt/freerdp-nightly/bin/xfreerdp3 /f /u:Quickemu /p:******** /v:<IP-address> /dynamic-resolution +decorations +fonts +aero +window-drag +multitransport +clipboard /floatbar:sticky:off /bpp:32 /audio-mode:0 /rfx /gfx:rfx /codec-cache:rfx /video /tune:FreeRDP_HiDefRemoteApp:true,FreeRDP_GfxAVC444v2:true,FreeRDP_GfxH264:true /scale-desktop:146 /scale-device:140
+    # /opt/freerdp-nightly/bin/xfreerdp3 /f /u:Quickemu /p:******** /v:"$(hostname -I | awk '{print $1}')" /dynamic-resolution +decorations +fonts +aero +window-drag +multitransport +clipboard /floatbar:sticky:off /bpp:32 /audio-mode:0 /rfx /gfx:rfx /codec-cache:rfx /video /tune:FreeRDP_HiDefRemoteApp:true,FreeRDP_GfxAVC444v2:true,FreeRDP_GfxH264:true /scale-desktop:146 /scale-device:140
 
     break
   elif [[ $response =~ ^(n|N)=$ ]]
