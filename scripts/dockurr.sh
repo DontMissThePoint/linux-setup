2e1cd9f56ec9abe5a8f321539bb7e86ec003fab9
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-06-16 20:14:42 +0300

    Improve performance: usb passthrough kvm

--- scripts/dockurr.sh
+++ scripts/dockurr.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# echo -n "Username: "
-# read username
+# read -p "Username: " username
 username=Quickemu
-read -p "Password: " password
+echo Username: "$username"
+stty -echo
+read -p "Password: " password; echo
+stty echo
 
 # Start windows inside Docker container
 /opt/freerdp-nightly/bin/xfreerdp3 /f /u:"${username}" /p:"${password}" /v:"$(hostname -I | awk '{print $1}')" /dynamic-resolution +decorations +fonts +aero +window-drag +multitransport +clipboard /floatbar:sticky:off /bpp:32 /audio-mode:0 /rfx /gfx:rfx /codec-cache:rfx /video /tune:FreeRDP_HiDefRemoteApp:true,FreeRDP_GfxAVC444v2:true,FreeRDP_GfxH264:true /scale-desktop:146 /scale-device:140
