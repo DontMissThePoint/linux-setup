a334e53b25771f3389274c82c848f7bf1d1872bb
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-09-08 22:23:54 +0300

    Work on code related to authorization, roles and permissions: android container

--- scripts/redroid.sh
+++ scripts/redroid.sh
@@ -1,31 +1,51 @@
 #!/bin/sh
 
-# get host IP
+# host
 IP_ADDRESS="$(hostname -I | awk '{print $1}')":11101
 
 # start the container
 cd ~/VirtualMachines/Android-Docker
-sudo docker compose up -d
+docker compose up -d
 
 # scrcpy-web
-sudo docker start `docker ps -a | grep 'scrcpy-web' | awk '{print $1}'` || \
-sudo docker run -itd --privileged -p 8000:8000/tcp emptysuns/scrcpy-web:v0.1
+until docker run -itd --privileged --name scrcpy-web -p 8000:8000/tcp emptysuns/scrcpy-web:v0.1
+do
+  # retry
+  echo Connection refused, retrying in 10 seconds...
+  docker rm -f `docker ps -a | grep 'scrcpy-web' | awk '{print $1}'`
+  sleep 10
+done
+
 
 # connect to android
-sudo docker exec -it scrcpy-web adb connect $IP_ADDRESS
+docker exec -it scrcpy-web adb connect $IP_ADDRESS
+
+# wireless display
+(scrcpy --tcpip=$IP_ADDRESS --audio-codec=raw &)
 
 # options
 adb -s $IP_ADDRESS shell settings put system accelerometer_rotation 0  #disable auto-rotate
 adb -s $IP_ADDRESS shell settings put system user_rotation 0  # 3 => 270° clockwise
-adb -s $IP_ADDRESS shell settings put global development_settings_enabled 1
-# adb -s $IP_ADDRESS emu geo fix [longitude] [latitude] [altitude]
 
-until scrcpy --tcpip=$IP_ADDRESS --audio-codec=raw
-do
-  # retry
-  echo Connection refused, retrying in 10 seconds...
-  sleep 10
-done
+# gps
+# adb -s $IP_ADDRESS shell settings put global development_settings_enabled 1
+adb -s $IP_ADDRESS shell settings put secure location_providers_allowed +gps,network
+
+# mock
+# adb -s $IP_ADDRESS emu geo fix [longitude] [latitude] [altitude]
+adb -s $IP_ADDRESS shell appops set com.lexa.fakegps android:mock_location allow
+adb -s $IP_ADDRESS shell settings put secure location_mode 3 # 0 => disable
+
+# service
+adb -s $IP_ADDRESS shell am start-foreground-service com.lexa.fakegps/.FakeGPSService
+
+# 3rd-party apps list
+# adb -s $IP_ADDRESS shell pm list packages -3
+
+# activities
+# adb shell dumpsys package | grep -Eo "^[[:space:]]+[0-9a-f]+[[:space:]]+com.lexa.fakegps/[^[:space:]]+" | grep -oE "[^[:space:]]+$"
+# adb -s $IP_ADDRESS shell monkey --pct-syskeys 0 -p com.lexa.fakegps -c android.intent.category.LAUNCHER 1
+# adb -s $IP_ADDRESS shell am start -a android.intent.action.MAIN -n com.lexa.fakegps/.ui.Main &
 
 # Open your browser,and open your_ip:8000. Click on the H264 Converter
 
