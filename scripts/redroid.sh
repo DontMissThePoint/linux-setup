20e23321476ebcfbf128fe68d80ce65ca45c1053
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2024-09-10 21:47:02 +0300

    Fix a bug: vim load ycm engine last

--- scripts/redroid.sh
+++ scripts/redroid.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# host
-IP_ADDRESS="$(hostname -I | awk '{print $1}')":11101
+# host IP address
+deviceID="$(hostname -I | awk '{print $1}')":11101
 
 # start the container
 cd ~/VirtualMachines/Android-Docker
@@ -18,34 +18,34 @@ done
 
 
 # connect to android
-docker exec -it scrcpy-web adb connect $IP_ADDRESS
+docker exec -it scrcpy-web adb connect $deviceID
 
 # wireless display
-(scrcpy --tcpip=$IP_ADDRESS --audio-codec=raw &)
+(scrcpy --tcpip=$deviceID --audio-codec=raw &)
 
 # options
-adb -s $IP_ADDRESS shell settings put system accelerometer_rotation 0  #disable auto-rotate
-adb -s $IP_ADDRESS shell settings put system user_rotation 0  # 3 => 270° clockwise
+adb -s $deviceID shell settings put system accelerometer_rotation 0  #disable auto-rotate
+adb -s $deviceID shell settings put system user_rotation 0  # 3 => 270° clockwise
 
 # gps
-# adb -s $IP_ADDRESS shell settings put global development_settings_enabled 1
-adb -s $IP_ADDRESS shell settings put secure location_providers_allowed +gps,network
+# adb -s $deviceID shell settings put global development_settings_enabled 1
+adb -s $deviceID shell settings put secure location_providers_allowed +gps,network
 
 # mock
-# adb -s $IP_ADDRESS emu geo fix [longitude] [latitude] [altitude]
-adb -s $IP_ADDRESS shell appops set com.lexa.fakegps android:mock_location allow
-adb -s $IP_ADDRESS shell settings put secure location_mode 3 # 0 => disable
+# adb -s $deviceID emu geo fix [longitude] [latitude] [altitude]
+adb -s $deviceID shell appops set com.lexa.fakegps android:mock_location allow
+adb -s $deviceID shell settings put secure location_mode 3 # 0 => disable
 
 # service
-adb -s $IP_ADDRESS shell am start-foreground-service com.lexa.fakegps/.FakeGPSService
+adb -s $deviceID shell am start-foreground-service com.lexa.fakegps/.FakeGPSService
 
 # 3rd-party apps list
-# adb -s $IP_ADDRESS shell pm list packages -3
+# adb -s $deviceID shell pm list packages -3
 
 # activities
 # adb shell dumpsys package | grep -Eo "^[[:space:]]+[0-9a-f]+[[:space:]]+com.lexa.fakegps/[^[:space:]]+" | grep -oE "[^[:space:]]+$"
-# adb -s $IP_ADDRESS shell monkey --pct-syskeys 0 -p com.lexa.fakegps -c android.intent.category.LAUNCHER 1
-# adb -s $IP_ADDRESS shell am start -a android.intent.action.MAIN -n com.lexa.fakegps/.ui.Main &
+# adb -s $deviceID shell monkey --pct-syskeys 0 -p com.lexa.fakegps -c android.intent.category.LAUNCHER 1
+# adb -s $deviceID shell am start -a android.intent.action.MAIN -n com.lexa.fakegps/.ui.Main &
 
 # Open your browser,and open your_ip:8000. Click on the H264 Converter
 
