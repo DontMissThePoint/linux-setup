79ed80e9222f42eda982efb84b839dfbb630fc08
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2025-01-07 17:03:23 +0300

    Fix a typo: restic backup

--- scripts/backup_hdd.sh
+++ scripts/backup_hdd.sh
@@ -5,7 +5,7 @@ RESTIC_REPOSITORY=/media/$USER/AUTORESTIC/07.OS/linux-setup
 LINUX_SETUP=$GIT_PATH/linux-setup
 
 # init
-[ ! -e "$RESTIC_REPOSITORY/linux-setup" ] && restic init --repo $RESTIC_REPOSITORY --password-file $RESTIC_PASSWORD_FILE
+[ ! -e "$RESTIC_REPOSITORY" ] && restic init --repo $RESTIC_REPOSITORY --password-file $RESTIC_PASSWORD_FILE
 
 # backup
 restic -r $RESTIC_REPOSITORY --verbose --password-file $RESTIC_PASSWORD_FILE backup $LINUX_SETUP
