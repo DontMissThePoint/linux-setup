235221ba7c2df153e16350d154a36122d91c0dd4
Shafiq Kigudde <kiguddeshafiq@gmail.com>
2026-05-08 01:02:07 +0300

    Fix typos: journal

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Detect device
 PHONE=$(mtp-detect 2>/dev/null | grep "Model:" | cut -d':' -f2 | xargs)
 
@@ -24,30 +28,29 @@ if [ "$PHONE" = "" ]; then
             echo "The container $DROID is not running."
 
             # Wait for the container
-            echo "Waiting for ReDroid to boot..."
+            echo "Waiting for ReDroid to resume..."
 
             # Start the container if it is not running
             docker start "$DROID"
-            sleep 3
+            sleep 1
         fi
     else
         echo "The container $DROID does not exist."
 
         # Create and start the container if it does not exist
-        docker run -d --name  redroid11
-
-        ReDroid
-        docker run -d \
+        docker run -itd --rm \
             --name "$DROID" \
             --privileged \
-            -p 5556:5555 \
+            -p 5552:5555 \
+            -p 5900:5900 \
             -v /opt/redroid/data:/data \
             redroid/redroid:11.0.0_litegapps_ndk_magisk_widevine \
             androidboot.redroid_width=1600 \
             androidboot.redroid_height=1600 \
             androidboot.redroid_dpi=480 \
-            androidboot.redroid_fps=60 \
-            androidboot.redroid_gpu_mode=guest \
+            redroid.fps=60 \
+            redroid.gpu.mode=guest \
+            redroid.vncserver=1 \
             ro.product.cpu.abilist=x86_64,arm64-v8a,x86,armeabi-v7a,armeabi \
             ro.product.cpu.abilist64=x86_64,arm64-v8a \
             ro.product.cpu.abilist32=x86,armeabi-v7a,armeabi \
@@ -69,11 +72,19 @@ else
     adb shell settings put system user_rotation 3  # 270° clockwise
 fi
 
-# connect
-adb connect localhost:5556
-
 # display
-scrcpy --video-codec=h264 --video-encoder=OMX.google.h264.encoder \
+until
+scrcpy --serial 127.0.0.1:5552 --video-codec=h264 --video-encoder=OMX.google.h264.encoder \
     --audio-codec=aac --audio-encoder=OMX.google.aac.encoder
 # --max-size 1920 --window-borderless
 # scrcpy -w --max-size 1600 --no-mouse-hover --window-borderless --window-y 0 # -S screen off save power
+do
+
+    # connect
+    adb kill-server
+
+    sleep 1
+    adb connect 127.0.0.1:5552
+
+    echo "Connect ... ${GREEN} ✔ ${NC}"
+done
